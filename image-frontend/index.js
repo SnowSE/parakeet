74edcace601e3f4cf9189e374a4048372ee7c39a
Alex Mickelson <alex.mickelson@ad.snow.edu>
2019-12-03 16:47:45 -0700

    added ppost request to front end

--- image-frontend/index.js
+++ image-frontend/index.js
@@ -8,5 +8,25 @@ app.get('/', function(req, res) {
     console.log("served index.html");
 });
 
+app.get('/image-url', function(req, res) {
+    console.log("recieved image url")
+    console.log(req.body)
+    request.post(process.env.OCR_API_URL, {
+        json: {
+            "imageurl":req.body.imageurl
+        }
+        }, (error, res, body) => {
+        if (error) {
+            console.error(error)
+            return
+        }
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(body)
+        })
+
+    //res.sendFile(path.join(__dirname + '/index.html'));
+    //console.log("served index.html");
+});
+
 app.listen(80);
 console.log("started server, listening on port 80");
