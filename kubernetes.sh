5da0eccc0e0149961d10ac7f0a1fce6c53451f92
Alex Mickelson <alexmickelson96@gmail.com>
2019-12-04 14:39:18 -0700

    Annotated kubernetes script

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,18 +1,13 @@
+#!/bin/bash
+
+# basic update
 apt update
 apt upgrade
+
+# need docker
 apt install docker.io
 
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
-
-kubeadm config images 
-
+#configure docker for kubeadm compatibility
 cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
@@ -23,9 +18,20 @@ cat > /etc/docker/daemon.json <<EOF
   "storage-driver": "overlay2"
 }
 EOF
-
 systemctl enable docker
-
 systemctl restart docker
-
+#disable swap for kubeadm compatibility
 swapoff -a 
+
+#install kubeacm
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# should pull images if everything worked
+kubeadm config images pull
