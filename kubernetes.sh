cf7cc5f355fd5e67fe8f3130cd243d4872344388
Alex Mickelson <alex.mickelson@ad.snow.edu>
2019-12-04 14:27:20 -0700

    added kubernetes script

---
+++ kubernetes.sh
@@ -0,0 +1,31 @@
+apt update
+apt upgrade
+apt install docker.io
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+kubeadm config images 
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+systemctl enable docker
+
+systemctl restart docker
+
+swapoff -a 
