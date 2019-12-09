360c4502305c3373d550bd997fc98cab9c10e312
Jonathan Allen <jallen@snow.edu>
2019-12-09 08:45:37 -0700

    Add smarts to auto-deflate gzip'd responses

--- SampleProducer/Program.cs
+++ SampleProducer/Program.cs
@@ -19,36 +19,62 @@ namespace SampleProducer
             var batchSize = int.Parse(args[2]);
 
             Console.WriteLine($"Starting with {startingNum}, doing {batchSize} @ {parserApiUrl}");
-            Console.WriteLine("Press [Enter] when you're ready to begin...");
-            Console.ReadLine();
+            //Console.WriteLine("Press [Enter] when you're ready to begin...");
+            //Console.ReadLine();
 
             var trygramService = RestService.For<ITrygramParserApi>(parserApiUrl);
 
             for(var num = startingNum; num < startingNum+batchSize; num++)
             {
-                var url = $"http://www.gutenberg.org/cache/epub/{num}/pg{num}.txt";
-                using (var client = new WebClient())
+                (var title, var text) = await downloadText(num);
+                if(title == null || text == null)
                 {
-                    try
-                    {
-                        var text = client.DownloadString(url).Substring(0, 10_000);
-                        var title = text.Substring(0, text.IndexOf('\n'));
-
-                        Console.WriteLine($"Submitting {title} (#{num} of {startingNum + batchSize})");
-                        await trygramService.CreateTrygramsAsync(new CreateTrygramRequest
-                        {
-                            Title = title,
-                            Text = text
-                        });
-                    }
-                    catch(Exception ex)
-                    {
-                        Console.WriteLine($"!! Unfortunately, #{num} died: {ex.Message}");
-                    }
+                    Console.WriteLine($"#{num} didn't return any title/text.  Skipping...");
+                    continue;
                 }
+
+                Console.WriteLine($"Submitting {title} (#{num} of {startingNum + batchSize})");
+
+                await addTrygrams(title, text, trygramService);
             }
 
             Console.WriteLine("All done!");
+        }
+
+        private static async Task<(string title, string text)> downloadText(int num)
+        {
+            var url = $"http://www.gutenberg.org/cache/epub/{num}/pg{num}.txt";
+            using (var client = new AutoDeflateWebClient())
+            {
+                try
+                {
+                    var text = await client.DownloadStringTaskAsync(url);                    
+                    text = text.Substring(0, 10_000);
+                    var title = text.Substring(0, text.IndexOf('\n'));
+                    return (title, text);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"!! Unable to download #{num}: {ex.Message}");
+                    return (null, null);
+                }
+            }
+        }
+
+        private static async Task addTrygrams(string title, string text, ITrygramParserApi trygramService)
+        {
+            try
+            {
+                await trygramService.CreateTrygramsAsync(new CreateTrygramRequest
+                {
+                    Title = title,
+                    Text = text
+                });
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"!! Unable to add trygrams for {title}: {ex.Message}");
+            }
         }
     }
 
@@ -62,5 +88,15 @@ namespace SampleProducer
     {
         public string Title { get; set; }
         public string Text { get; set; }
+    }
+
+    public class AutoDeflateWebClient : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            HttpWebRequest request = base.GetWebRequest(address) as HttpWebRequest;
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+            return request;
+        }
     }
 }
